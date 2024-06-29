2bd399f64f46ca4cbf2205ba624c5013e5546796
Gunjan Jain <jain10gunjan@gmail.com>
2024-06-29 20:02:12 +0530

    Added Monetag Verification Meta Tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,10 @@ export const metadata: Metadata = {
   title: "SrtLink",
   description: "A Casual but Trustable Short Link on the Internet.",
 };
+
+<Head>
+  <meta name="monetag" content="56933abcf7d0bdacbd68d2e0399eb34f" />
+</Head>
 
 export default function RootLayout({
   children,
@@ -18,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}</body>
     </html>
   );
